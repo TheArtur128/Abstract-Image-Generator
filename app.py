b63216aad59dd23fccef8482a94f3bbe2f49f73b
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-07-03 20:33:51 +0700

    Updated URI

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from resources import RandomImage, RandomImageToken, RandomFavicon
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(RandomImage, '/', "/<string:token>", "/random-image/<string:token>")
+api.add_resource(RandomImage, '/', "/<string:token>", "/random-image", "/random-image/<string:token>")
 api.add_resource(RandomImageToken, "/random-image-token")
 api.add_resource(RandomFavicon, "/favicon.ico")
 
