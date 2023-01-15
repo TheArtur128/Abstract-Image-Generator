65da5c1617223079557a3e40ca4132769c7e3821
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-15 15:54:17 +0700

    Removing local deployment

--- app.py
+++ app.py
@@ -10,6 +10,3 @@ api = Api(app)
 api.add_resource(RandomImage, '/', "/<string:token>", "/random-image", "/random-image/<string:token>")
 api.add_resource(RandomImageToken, "/random-image-token")
 api.add_resource(RandomFavicon, "/favicon.ico")
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8010)
